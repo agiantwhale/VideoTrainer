3dae4a241243c237106207659c3e60ea97885061
Il Jae Lee <iljae@umich.edu>
2015-09-27 11:16:21 -0400

    Housekeeping.

--- main.cpp
+++ main.cpp
@@ -150,33 +150,34 @@ int main() {
   std::vector<maav::Features> features_collection;
   std::vector<unsigned int> divider;
 
-  maav::HOGExtractor extractor(window_size);
+  //maav::HOGExtractor extractor(window_size);
 
-  maav::FeatureExtractMethod extract_method(extractor, features_collection);
-  boost::function<void (const cv::Mat &)> f=boost::ref(extract_method);
+  //maav::FeatureExtractMethod extract_method(extractor, features_collection);
+  //boost::function<void (const cv::Mat &)> f=boost::ref(extract_method);
 
-  maav::LoadEachFrameFromFile("/Users/iljae/Development/MHackers/data/positive.MOV", f);
-  for(unsigned int i=0;i<(features_collection.size()-divider.size());i++) {
-    divider.push_back(0);
-  }
+  //maav::LoadEachFrameFromFile("/Users/iljae/Development/MHackers/data/positive.MOV", f);
+  //for(unsigned int i=0;i<(features_collection.size()-divider.size());i++) {
+  //  divider.push_back(0);
+  //}
 
-  std::cout << "Positive extraction is done!" << std::endl;
+  //std::cout << "Positive extraction is done!" << std::endl;
 
-  extractor.scale_=false;
-  maav::LoadEachFrameFromFile("/Users/iljae/Development/MHackers/data/negative.MOV", f);
+  //extractor.scale_=false;
+  //maav::LoadEachFrameFromFile("/Users/iljae/Development/MHackers/data/negative.MOV", f);
 
-  std::cout << "Negative extraction is done!" << std::endl;
+  //std::cout << "Negative extraction is done!" << std::endl;
+
+  //{
+  //  std::ofstream file_dump("/Users/iljae/Development/MHackers/data/features.dump", std::ofstream::binary);
+  //  cereal::BinaryOutputArchive oarchive(file_dump);
+  //  oarchive(features_collection, divider);
+  //}
 
   {
-    std::ofstream file_dump("/Users/iljae/Development/MHackers/data/features.dump", std::ofstream::binary);
-    cereal::BinaryOutputArchive oarchive(file_dump);
-    oarchive(features_collection, divider);
+    std::ifstream file_dump("/Users/iljae/Development/MHackers/data/features.dump", std::ifstream::binary);
+    cereal::BinaryInputArchive iarchive(file_dump);
+    iarchive(features_collection, divider);
   }
-  // {
-  //   std::ifstream file_dump("/Users/iljae/Development/MHackers/data/features.dump", std::ifstream::binary);
-  //   cereal::BinaryInputArchive iarchive(file_dump);
-  //   iarchive(features_collection, divider);
-  // }
 
   maav::NeuralNet learner(3, (features_collection.back().size()+divider.back()+2)/2, 50);
 
